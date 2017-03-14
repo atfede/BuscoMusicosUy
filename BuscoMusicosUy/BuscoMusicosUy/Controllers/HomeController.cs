d64a704c275707e3169ec04422735cb7f965f8e5
Federico Testoni <eltesto@hotmail.com>
2017-03-13 21:55:48 -0300

    Added search

--- BuscoMusicosUy/BuscoMusicosUy/Controllers/HomeController.cs
+++ BuscoMusicosUy/BuscoMusicosUy/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BuscoMusicosUy.Controllers
+namespace Musicos.Controllers
 {
     public class HomeController : Controller
     {
@@ -13,16 +13,9 @@ namespace BuscoMusicosUy.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult SearchUsers()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
